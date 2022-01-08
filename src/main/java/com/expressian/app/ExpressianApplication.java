a067a8bc23e15a4de18aba201b9cd1d225137e54
unknown <22105805+enjee1@users.noreply.github.com>
2022-01-08 16:11:52 -0500

    Created models, controllers, and repositories packages structure. Created starting files for Customer and Vehicle entities

--- src/main/java/com/expressian/app/ExpressianApplication.java
+++ src/main/java/com/expressian/app/ExpressianApplication.java
@@ -1,4 +1,4 @@
-package com.example.expressian;
+package com.expressian.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/expressian/app/models/Vehicle.java
@@ -0,0 +1,4 @@
+package com.expressian.app.models;
+
+public class Vehicle {
+}
